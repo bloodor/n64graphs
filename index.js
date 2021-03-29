7a68e1a4be8658e942069d5827d0ba4f90377973
Milo <florentin.roberge@gmail.com>
2021-03-29 09:15:24 +0200

    Add base logo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { FBXLoader } from '../loaders/FBXLoader.js'
+import { FBXLoader } from '/loaders/FBXLoader.js'
 
 // Create scene, camera, renderer
 const scene = new THREE.Scene();
@@ -8,21 +8,24 @@ const renderer = new THREE.WebGLRenderer({ antialias: true});
 //Mesh Import
 const loader = new FBXLoader();
 
-loader.load( 'logo.fbx', function ( object ) {
-
-    scene.add( object );
-
+loader.load( '../assets/logo.fbx', function ( object ) {
+    const ncube = object;
+    scene.add( ncube );
+    ncube.scale.set(.01, .01, .01)
 } );
 
 // Apply the size of the window for the renderer
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
-camera.position.y = 2
-camera.position.z = 2
+camera.position.x = 15
+camera.position.y = 10
+camera.position.z = 15
+
+camera.rotation.y = degrees_to_radians(45)
 
 // Make lights
-var ambiantLight = new THREE.AmbientLight ( 0xffffff, 0.5)
+var ambiantLight = new THREE.AmbientLight ( 0xffffff, 1)
 scene.add(ambiantLight)
 
 var pointLight = new THREE.PointLight(0xffffff, 1);
@@ -36,3 +39,9 @@ function animate() {
     renderer.render( scene, camera )
 }
 animate()
+
+function degrees_to_radians(degrees)
+{
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
